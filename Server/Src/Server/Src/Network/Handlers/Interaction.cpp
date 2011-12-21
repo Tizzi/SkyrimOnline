79cff8425320a627613cb31d95e45f0048e6f300
Maxime Griot <yamashi@live.com>
2011-12-21 03:32:03 +0000

    Licence infos

--- Server/Src/Server/Src/Network/Handlers/Interaction.cpp
+++ Server/Src/Server/Src/Network/Handlers/Interaction.cpp
@@ -1,3 +1,13 @@
+/**
+THE WORK (AS DEFINED BELOW) IS PROVIDED UNDER THE TERMS OF THIS CREATIVE COMMONS PUBLIC LICENSE ("CCPL" OR "LICENSE").
+THE WORK IS PROTECTED BY COPYRIGHT AND/OR OTHER APPLICABLE LAW.
+ANY USE OF THE WORK OTHER THAN AS AUTHORIZED UNDER THIS LICENSE OR COPYRIGHT LAW IS PROHIBITED.
+
+BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE TO BE BOUND BY THE TERMS OF THIS LICENSE.
+TO THE EXTENT THIS LICENSE MAY BE CONSIDERED TO BE A CONTRACT,
+THE LICENSOR GRANTS YOU THE RIGHTS CONTAINED HERE IN CONSIDERATION OF YOUR ACCEPTANCE OF SUCH TERMS AND CONDITIONS.
+*/
+
 #include "stdafx.h"
 #include <Network/Session.h>
 #include <Network/Server.h>
@@ -92,21 +102,18 @@ namespace Skyrim
 				Packet packet;
 				packet.Opcode = SMSG_PLAYER_MOUNT_SPAWN;
 				packet.Push(GetId());
-				packet.Push(GetPlayer().GetMount()->GetSkyrimId());
+				packet.Push(pData.Int(0));
 				DispatchInRange(packet);
 			}
 		}
 		//---------------------------------------------------------------------
 		void Session::HandleUnmount(Packet& pData)
 		{
-			if(pData.Int(0) == 0)
-			{
-				GetPlayer().SetMount(0);
-				Packet packet;
-				packet.Opcode = SMSG_PLAYER_MOUNT_REMOVE;
-				packet.Push(GetId());
-				DispatchInRange(packet);
-			}
+			GetPlayer().SetMount(0);
+			Packet packet;
+			packet.Opcode = SMSG_PLAYER_MOUNT_REMOVE;
+			packet.Push(GetId());
+			DispatchInRange(packet);
 		}
 		//---------------------------------------------------------------------
 		float Session::GetDistance(Session::pointer pPlayer)
--- Server/Src/Server/Src/Network/Packet.h
+++ Server/Src/Server/Src/Network/Packet.h
@@ -1,3 +1,13 @@
+/**
+THE WORK (AS DEFINED BELOW) IS PROVIDED UNDER THE TERMS OF THIS CREATIVE COMMONS PUBLIC LICENSE ("CCPL" OR "LICENSE").
+THE WORK IS PROTECTED BY COPYRIGHT AND/OR OTHER APPLICABLE LAW.
+ANY USE OF THE WORK OTHER THAN AS AUTHORIZED UNDER THIS LICENSE OR COPYRIGHT LAW IS PROHIBITED.
+
+BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE TO BE BOUND BY THE TERMS OF THIS LICENSE.
+TO THE EXTENT THIS LICENSE MAY BE CONSIDERED TO BE A CONTRACT,
+THE LICENSOR GRANTS YOU THE RIGHTS CONTAINED HERE IN CONSIDERATION OF YOUR ACCEPTANCE OF SUCH TERMS AND CONDITIONS.
+*/
+
 #pragma once
 
 #include <boost/archive/text_iarchive.hpp>
@@ -57,21 +67,21 @@ namespace Skyrim
 			/// @param n The value's index
 			/// @return The value
 			//--------------------------------------------------------------------//
-			unsigned int					Int(int n){return mIntData[n];}
+			unsigned int					Int(int n){return mIntData.at(n);}
 
 			//--------------------------------------------------------------------//
 			/// @brief Returns a float value
 			/// @param n The value's index
 			/// @return The value
 			//--------------------------------------------------------------------//
-			float					Float(int n){return mFloatData[n];}
+			float					Float(int n){return mFloatData.at(n);}
 
 			//--------------------------------------------------------------------//
 			/// @brief Returns a wstring value
 			/// @param n The value's index
 			/// @return The value
 			//--------------------------------------------------------------------//
-			std::string			String(int n){return mString[n];}
+			std::string			String(int n){return mString.at(n);}
 
 			//--------------------------------------------------------------------//
 			/// @brief Add a value to the packet
